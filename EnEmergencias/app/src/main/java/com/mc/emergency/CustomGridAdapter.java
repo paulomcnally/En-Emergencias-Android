b30734f1fa6f7577706b14f5f9d7a99be6c72810
Paulo McNally <paulomcnally@gmail.com>
2014-04-14 03:11:32 -0600

    0.0.0

---
+++ EnEmergencias/app/src/main/java/com/mc/emergency/CustomGridAdapter.java
@@ -0,0 +1,69 @@
+package com.mc.emergency;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+/**
+ * Created by paulomcnally on 4/12/14.
+ */
+public class CustomGridAdapter extends BaseAdapter {
+
+    private Context context;
+    private String[] texts;
+    private String[] images;
+    LayoutInflater inflater;
+
+    public CustomGridAdapter(Context context, String[] texts, String[] images) {
+        this.context = context;
+        this.texts = texts;
+        this.images = images;
+        inflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.grid_item, null);
+        }
+
+        TextView text = (TextView) convertView.findViewById( R.id.grid_item_text );
+        text.setText(texts[position]);
+
+        int identifier = context.getResources().getIdentifier(images[position], "drawable", context.getPackageName());
+
+        ImageView image = (ImageView) convertView.findViewById( R.id.grid_item_image );
+        image.setBackgroundResource(identifier);
+
+
+        //convertView.setLayoutParams(new GridView.LayoutParams(GridView.AUTO_FIT, rowHigh));
+
+        //Button button = (Button) convertView.findViewById(R.id.grid_item);
+        //button.setText(items[position]);
+
+        return convertView;
+    }
+
+    @Override
+    public int getCount() {
+        return texts.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return texts[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+}
---
+++ EnEmergencias/app/src/main/java/com/mc/emergency/MainActivity.java
@@ -0,0 +1,80 @@
+package com.mc.emergency;
+
+import android.content.res.Resources;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ExpandableListView;
+import android.widget.GridView;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private ExpandableHeightGridView gridViewTypes;
+    private ExpandableHeightGridView gridViewNumbers;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Resources res = getResources();
+
+        // set types on gridView
+
+        gridViewTypes = (ExpandableHeightGridView) this.findViewById(R.id.mainGridViewTypes);
+        CustomGridAdapter gridAdapterTypes = new CustomGridAdapter(MainActivity.this, res.getStringArray(R.array.emergency_types),res.getStringArray(R.array.emergency_types_images));
+        gridViewTypes.setAdapter(gridAdapterTypes);
+        gridViewTypes.setExpanded(true);
+        gridViewTypes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //text.setText((String) (gridView.getItemAtPosition(position)));
+                Log.i("ITEM_CLICKED", "" + (String) (gridViewTypes.getItemAtPosition(position)));
+            }
+        });
+
+
+        gridViewNumbers = (ExpandableHeightGridView) this.findViewById(R.id.mainGridViewNumbers);
+        CustomGridAdapter gridAdapterNumbers = new CustomGridAdapter(MainActivity.this, res.getStringArray(R.array.emergency_numbers_text), res.getStringArray(R.array.emergency_numbers_images));
+        gridViewNumbers.setAdapter(gridAdapterNumbers);
+        gridViewNumbers.setExpanded(true);
+        gridViewNumbers.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //text.setText((String) (gridView.getItemAtPosition(position)));
+                Log.i("ITEM_CLICKED", "" + (String) (gridViewNumbers.getItemAtPosition(position)));
+            }
+        });
+
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
