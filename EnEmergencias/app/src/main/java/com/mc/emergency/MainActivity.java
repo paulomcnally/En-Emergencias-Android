d141fb5ec7a34a93246633e56a8795ffd0e90be0
Paulo McNally <paulomcnally@gmail.com>
2014-04-14 18:20:39 -0600

    Color item change

--- EnEmergencias/app/src/main/java/com/mc/emergency/MainActivity.java
+++ EnEmergencias/app/src/main/java/com/mc/emergency/MainActivity.java
@@ -2,6 +2,7 @@ package com.mc.emergency;
 
 import android.content.Intent;
 import android.content.res.Resources;
+import android.media.Image;
 import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -9,9 +10,12 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.widget.AdapterView;
 import android.widget.ExpandableListView;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -39,13 +43,30 @@ public class MainActivity extends ActionBarActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
+
+                ImageView i = (ImageView) parent.findViewById(R.id.grid_item_image);
+
+                AlphaAnimation alpha = new AlphaAnimation(0.5F, 0.5F); // change values as you want
+                alpha.setDuration(9);
+                alpha.setFillAfter(true);
+                i.startAnimation(alpha);
+
+
                 if( !res.getStringArray(R.array.emergency_types_pages)[position].equals("empty") ){
+
+
+
                     Intent intent = new Intent();
                     intent.setClass(getApplicationContext(), WebActivity.class);
                     intent.putExtra("page", res.getStringArray(R.array.emergency_types_pages)[position]);
                     intent.putExtra("title", res.getStringArray(R.array.emergency_types)[position]);
                     intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
                     getApplicationContext().startActivity(intent);
+
+                    AlphaAnimation alphaOut = new AlphaAnimation(1F, 1F); // change values as you want
+                    alphaOut.setDuration(0);
+                    alphaOut.setFillAfter(true);
+                    i.startAnimation(alphaOut);
                 }
                 else{
                     Toast.makeText(getApplicationContext(), res.getString(R.string.message_empty), Toast.LENGTH_LONG).show();
