07ac4c0aea958dad057e559f086dc032e3fd00e2
Paulo McNally <paulomcnally@gmail.com>
2014-04-14 18:26:24 -0600

    remove menu

--- EnEmergencias/app/src/main/java/com/mc/emergency/WebActivity.java
+++ EnEmergencias/app/src/main/java/com/mc/emergency/WebActivity.java
@@ -50,15 +50,6 @@ public class WebActivity extends ActionBarActivity {
 
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.web, menu);
-        return true;
-    }
-
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (event.getAction() == KeyEvent.ACTION_DOWN) {
